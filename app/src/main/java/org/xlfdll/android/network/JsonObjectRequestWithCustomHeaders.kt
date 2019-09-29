876be1ed6155097745764005ac9f9b5031a809f4
xlfdll <xlfdll@gmail.com>
2019-09-29 03:00:57 -0700

    Updated Android network library code

--- app/src/main/java/org/xlfdll/android/network/JsonObjectRequestWithCustomHeaders.kt
+++ app/src/main/java/org/xlfdll/android/network/JsonObjectRequestWithCustomHeaders.kt
@@ -7,15 +7,26 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import org.json.JSONObject
 
-public class JsonObjectRequestWithCustomHeaders(
+class JsonObjectRequestWithCustomHeaders(
     method: Int,
     url: String,
-    val customHeaders: MutableMap<String, String>?,
+    private val customHeaders: MutableMap<String, String>?,
     jsonRequest: JSONObject,
     listener: Response.Listener<JSONObject>,
     errorListener: Response.ErrorListener
-) :
-    JsonObjectRequest(method, url, jsonRequest, listener, errorListener) {
+) : JsonObjectRequest(method, url, jsonRequest, listener, errorListener) {
+    init {
+        if (customHeaders != null) {
+            val originalHeaders = super.getHeaders()
+
+            for (headerEntry in originalHeaders) {
+                if (!customHeaders.containsKey(headerEntry.key)) {
+                    customHeaders[headerEntry.key] = headerEntry.value
+                }
+            }
+        }
+    }
+
     override fun getHeaders(): MutableMap<String, String> {
         return customHeaders ?: super.getHeaders()
     }
