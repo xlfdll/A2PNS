ae911a31d0dba26882e90c211bb5505eda53f1dc
xlfdll <xlfdll@gmail.com>
2020-01-10 23:51:38 -0800

    Fixed incorrect serialized variable names in APNSAuthToken class after obfuscation

--- app/src/main/java/org/xlfdll/a2pns/models/apple/APNSAuthToken.kt
+++ app/src/main/java/org/xlfdll/a2pns/models/apple/APNSAuthToken.kt
@@ -1,7 +1,15 @@
 package org.xlfdll.a2pns.models.apple
 
+import com.google.gson.annotations.SerializedName
+
 class APNSAuthToken(
+    // Must add @SerializedName annotations for all variables used by Gson
+    // In release builds, obfuscation will replace all variable names
+    // Thus, the serialized names become incorrect
+    @SerializedName("id")
     var id: String,
+    @SerializedName("iat")
     var iat: Long,
+    @SerializedName("jwt")
     var jwt: String
 )
